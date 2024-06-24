0cd95dca2437f3facbf76a3757f754900ca27d5a
stanleyl321 <63362378+stanleyl321@users.noreply.github.com>
2024-06-24 14:10:33 -0400

    Add files via upload

---
+++ summer_project.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 24 13:53:38 2024
+
+@author: stanleyliu
+"""
+import json
+file = open("sharc1-official/json/sharc_train.json")
+data = json.load(file)
